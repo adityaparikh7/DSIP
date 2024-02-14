3ee355574e38f43d16561c9a90a99ca5c7f009bb
Aditya Parikh <asparikh03@gmail.com>
2024-02-14 23:12:13 +0530

    added gray level slicing

--- exp 5/main.m
+++ exp 5/main.m
@@ -7,11 +7,11 @@ img_neg = L - img;
 % Display images in a 2x2 grid
 figure;
 
-subplot(2, 2, 1);
+subplot(2, 3, 1);
 imshow(img);
 title('Original Image');
 
-subplot(2, 2, 2);
+subplot(2, 3, 2);
 imshow(uint8(img_neg));
 title('Negative Image');
 
@@ -28,7 +28,7 @@ for i = 1:m
     end
 end
 
-subplot(2, 2, 3);
+subplot(2, 3, 3);
 imshow(uint8(img_thresh));
 title('Threshold Without Background');
 
@@ -46,9 +46,23 @@ for i = 1:m
     end
 end
 
-subplot(2, 2, 4);
+subplot(2, 3, 4);
 imshow(uint8(img_thresh_back));
 title('Threshold With Background');
 
+% Gray level slicing
+T1 = 100; % Lower threshold
+T2 = 150; % Upper threshold
 
+% Initialize the output image
+img_gray_slice = img;
+
+% Apply gray level slicing
+img_gray_slice(img >= T1 & img <= T2) = 255; % Set pixels in the range [T1, T2] to 255
+img_gray_slice(img < T1 | img > T2) = 0; % Set all other pixels to 0
+
+% Display the result
+subplot(2, 3, 5); % Position in the 2x3 grid
+imshow(img_gray_slice);
+title('Gray Level Slicing');
 
