ded7fed7a619ac6dbe53db5f3cffaaa72f0b193b
Aditya Parikh <asparikh03@gmail.com>
2024-02-16 19:07:12 +0530

    commit

--- exp 6/contraststretch.m
+++ exp 6/contraststretch.m
@@ -13,26 +13,54 @@ r2 = input('Enter r2 (maximum pixel value for stretching): ');
 s1 = input('Enter s1 (desired minimum output intensity): ');
 s2 = input('Enter s2 (desired maximum output intensity): ');
 
+% % Apply contrast stretching transformation
+% stretchedGrayImage = contrastStretching(originalGrayImage, r1, r2, s1, s2);
+
 % Apply contrast stretching transformation
-stretchedGrayImage = contrastStretching(originalGrayImage, r1, r2, s1, s2);
+[stretchedGrayImage, slope, inputIntensity] = contrastStretching(originalGrayImage, r1, r2, s1, s2);
 
 % Display the original and stretched images
 figure;
-subplot(1, 3, 1);
+subplot(2, 2, 1);
 imshow(uint8(originalImage));
 title('Original Color Image');
 
-subplot(1, 3, 2);
+subplot(2, 2, 2);
 imshow(uint8(originalGrayImage));
 title('Original Grayscale Image');
 
-subplot(1, 3, 3);
+subplot(2, 2, 3);
 imshow(uint8(stretchedGrayImage));
 title('Contrast Stretched Grayscale Image');
 
-function stretchedImage = contrastStretching(inputImage, r1, r2, s1, s2)
+% Plot the slope
+subplot(2, 2, 4);
+plot(inputIntensity, slope, '-');
+title('Slope of Contrast Stretching Transformation');
+xlabel('Input Intensity');
+ylabel('Slope');
+grid on;
+
+% function stretchedImage = contrastStretching(inputImage, r1, r2, s1, s2)
+%     % Apply the contrast stretching transformation
+%     stretchedImage = (s2 - s1) / (r2 - r1) * (inputImage - r1) + s1;
+    
+%     % Clip values to be within the desired output range
+%     stretchedImage = max(min(stretchedImage, s2), s1);
+% end
+
+% Print the slope
+fprintf('The slope of the contrast stretching transformation is: %.4f\n', slope);
+
+function [stretchedImage, slope, inputIntensity] = contrastStretching(inputImage, r1, r2, s1, s2)
+    % Calculate the slope of the contrast stretching transformation
+    slope = (s2 - s1) / (r2 - r1);
+
+    % Generate input intensity values for plotting
+    inputIntensity = r1 : 1 : r2;
+    
     % Apply the contrast stretching transformation
-    stretchedImage = (s2 - s1) / (r2 - r1) * (inputImage - r1) + s1;
+    stretchedImage = slope * (inputImage - r1) + s1;
     
     % Clip values to be within the desired output range
     stretchedImage = max(min(stretchedImage, s2), s1);
