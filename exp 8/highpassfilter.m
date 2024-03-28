4ff852200ab23f2a5bfedf9322253eb5a7b72655
adityaparikh7 <asparikh03@gmail.com>
2024-03-28 11:55:16 +0530

    commit

---
+++ exp 8/highpassfilter.m
@@ -0,0 +1,20 @@
+image = imread('_MG_9276.JPG');
+
+% Convert to grayscale
+gray_image = rgb2gray(image);
+
+% high pass filter
+% filter = fspecial('laplacian', 0.5);
+filter = [0 -1 0; -1 5 -1; 0 -1 0];
+
+% Apply filter
+new_image = imfilter(gray_image, filter);
+
+figure;
+subplot(1, 2, 1);
+imshow(gray_image);
+title('Original Image');
+
+subplot(1, 2, 2);
+imshow(new_image);
+title('Sharpened Image');
---
+++ exp 8/lowpassfilter.m
@@ -0,0 +1,22 @@
+image = imread('_MG_9276.JPG');
+
+% Convert to grayscale
+gray_image = rgb2gray(image);
+
+% Define the filter
+size = 3;
+sigma = 1.5;
+filter_kernel = fspecial('gaussian', size, sigma);
+
+% Apply the filter
+new_image = imfilter(gray_image, filter_kernel, 'replicate');
+
+% Display the original and smoothed images
+figure;
+subplot(1, 2, 1);
+imshow(gray_image);
+title('Original Image');
+
+subplot(1, 2, 2);
+imshow(new_image);
+title('Smoothed Image');
